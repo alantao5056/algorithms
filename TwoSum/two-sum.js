d7210b2cb983e4c12caae8e3694087c25af0466b
alantao5056 <alantao5056@gmail.com>
2019-09-08 11:01:38 -0400

    optimized the loop's start and end value; added three more test cases;

--- TwoSum/two-sum.js
+++ TwoSum/two-sum.js
@@ -4,6 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] + nums[i] === target) {
+        return [j, i];
+      }
+    }
+  }
+  return [];
 };
 
 module.exports = twoSum;
--- TwoSum/two-sum.test.js
+++ TwoSum/two-sum.test.js
@@ -7,10 +7,30 @@ describe('two-sum', () => {
     expect(result).toIncludeAllMembers([4, 2]);
   });
 
-
   test('find from [4, 8, 1, 3, 9, 11, 5] with target 100', () => {
     const nums = [4, 8, 1, 3, 9, 11, 5];
-    const result = twoSum(nums, 10);
+    const result = twoSum(nums, 100);
     expect(result).toBeArrayOfSize(0);
   });
+
+  test('find the last two index', () => {
+    const nums = [0, 0, 0, 0, 0, 99, 1];
+    const result = twoSum(nums, 100);
+    expect(result).toIncludeAllMembers([5, 6]);
+  });
+
+  test('find the first two index', () => {
+    const nums = [2, 33, 4, 5, 6, 7, 8, 9];
+    const result = twoSum(nums, 35);
+    expect(result).toIncludeAllMembers([0, 1]);
+  });
+
+  test('find the last and first two index', () => {
+    const nums = [1, 4, 5, 6, 7, 83, 3, 5, 6, 1];
+    const result = twoSum(nums, 2);
+    expect(result).toIncludeAllMembers([0, 9]);
+  });
+
 });
+
+
