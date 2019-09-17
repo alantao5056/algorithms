5704bf7b4dae82b3798cbe1cb090ee6dabb488c8
alantao5056 <alantao5056@gmail.com>
2019-09-16 20:50:02 -0400

    completed palindrome

--- Palindrome/palindrome.js
+++ Palindrome/palindrome.js
@@ -3,7 +3,23 @@
  * @return {boolean}
  */
 var isPalindrome = function (word) {
+  if (word === '') {
+    return false;
+  }
+
+  let i = 0;
+  let j = word.length - 1;
+
+  while (i < j) {
+    if (word[i] !== word[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+
   return true;
+
 };
 
 module.exports = isPalindrome;
