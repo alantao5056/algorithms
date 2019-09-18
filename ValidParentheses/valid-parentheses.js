b546f9a9fb3a99c4c42ba576e80dd70754d3e4c0
alantao5056 <alantao5056@gmail.com>
2019-09-18 16:34:30 -0400

    finished valid-parentheses

--- ValidParentheses/valid-parentheses.js
+++ ValidParentheses/valid-parentheses.js
@@ -1,8 +1,48 @@
+class Stack {
+  constructor() {
+    this.array = [];
+  }
+
+  size() {
+    return this.array.length;
+  };
+
+  push(item) {
+    this.array.push(item);
+  };
+
+  peek() {
+    return this.array[this.array.length - 1];
+  }
+
+  pop() {
+    this.array.pop();
+  }
+};
+
 /**
  * @param {string} s
  * @return {boolean}
  */
 const isValid = function (s) {
+
+  const parentheses = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  };
+
+  const stack = new Stack();
+
+  for (let i = 0; i < s.length; i++) {
+    if (stack.peek() === s[i]) {
+      stack.pop();
+    } else {
+      stack.push(parentheses[s[i]]);
+    }
+  }
+
+  return stack.size() === 0;
 
 };
 
