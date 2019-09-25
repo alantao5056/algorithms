64742b368ec86582376539d33e33b2f3cfa848b3
alantao5056 <alantao5056@gmail.com>
2019-09-24 20:45:59 -0400

    finished happy-number

--- HappyNumber/happy-number.js
+++ HappyNumber/happy-number.js
@@ -3,6 +3,25 @@
  * @return {boolean}
  */
 var isHappy = function (n) {
+  let hash = {};
+  while (hash[n] === undefined) {
+    let sum = 0;
+    hash[n] = true;
+
+    while (n !== 0) {
+      let ones = n % 10;
+      n = (n - ones) / 10;
+      sum += ones * ones;
+    }
+
+    if (sum === 1) {
+      return true;
+    } else {
+      n = sum;
+    }
+  }
+
+  return false;
 
 };
 
