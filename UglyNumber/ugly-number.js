1bb2340e95a7918bf0439c0b435a3c086788dda3
alantao5056 <alantao5056@gmail.com>
2019-09-25 20:39:40 -0400

    finished ugly number

--- UglyNumber/ugly-number.js
+++ UglyNumber/ugly-number.js
@@ -3,7 +3,18 @@
  * @return {boolean}
  */
 const isUgly = function (num) {
-
+  while (num !== 1) {
+    if (num % 2 === 0) {
+      num /= 2;
+    } else if (num % 3 === 0) {
+      num /= 3;
+    } else if (num % 5 === 0) {
+      num /= 5;
+    } else {
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = isUgly;
