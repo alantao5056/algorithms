e2756ecad8ef4be26112706cef76e9711f593515
alantao5056 <alantao5056@gmail.com>
2019-09-26 19:43:12 -0400

    finished longest common prefix

--- LongestCommonPrefix/longest-common-prefix.js
+++ LongestCommonPrefix/longest-common-prefix.js
@@ -3,7 +3,28 @@
  * @return {string}
  */
 const longestCommonPrefix = function (strs) {
-
+  let prefix = '';
+  let minLength = shortestInt(strs);
+  for (let i = 0; i < minLength; i++) {
+    let j;
+    for (j = 0; j < strs.length - 1; j++) {
+      if (strs[j][i] !== strs[j + 1][i]) {
+        return prefix;
+      }
+    }
+    prefix += strs[j][i];
+  }
+  return prefix;
 };
+
+const shortestInt = function (strs) {
+  let num = strs[0].length;
+  for (let i = 1; i < strs.length; i++) {
+    if (strs[i].length < num) {
+      num = strs[i].length;
+    }
+  }
+  return num;
+}
 
 module.exports = longestCommonPrefix;
--- LongestCommonPrefix/longest-common-prefix.test.js
+++ LongestCommonPrefix/longest-common-prefix.test.js
@@ -3,7 +3,7 @@ const longestCommonPrefix = require('./longest-common-prefix');
 describe('longestCommonPrefix', () => {
 
   test('test fl prefix', () => {
-    expect(longestCommonPrefix(["flower", "flow", "flight"])).boBe('fl');
+    expect(longestCommonPrefix(["flower", "flow", "flight"])).toBe('fl');
   });
 
   test('test no prefix', () => {
