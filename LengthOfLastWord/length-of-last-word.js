4e4363e0ee07164cf370bf86fcab43388848d1c3
alantao5056 <alantao5056@gmail.com>
2019-10-01 19:45:42 -0400

    made task harder

--- LengthOfLastWord/length-of-last-word.js
+++ LengthOfLastWord/length-of-last-word.js
@@ -3,9 +3,20 @@
  * @return {number}
  */
 const lengthOfLastWord = function (s) {
-  s = s.trim();
-  const string = s.split(' ');
-  return string[string.length - 1].length;
+  let trim = false;
+  let result = '';
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === ' ' && trim === true) {
+      break;
+    } else if (s[i] !== ' ') {
+      result = s[i] + result;
+      console.log(result);
+      trim = true;
+    }
+  }
+  return result.length;
 };
+
+
 
 module.exports = lengthOfLastWord;
