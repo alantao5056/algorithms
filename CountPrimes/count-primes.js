6b14bf885e6359cc50c3e6dbc0258b46ee11d8e0
Alan Tao <alantao5056@gmail.com>
2019-11-19 16:03:18 -0500

    finished count primes

--- CountPrimes/count-primes.js
+++ CountPrimes/count-primes.js
@@ -3,7 +3,29 @@
  * @return {number}
  */
 const countPrimes = function (n) {
+  let count = 0;
+  let array = [];
 
+  for (let i = 2; i < n; i++) {
+    array.push(true);
+  }
+
+  for (let i = 2; i < n; i++) {
+    let iMultiply = 2;
+    if (array[i]) {
+      while (iMultiply * i <= n) {
+        array[i * iMultiply] = false;
+        iMultiply++;
+      }
+    }
+  }
+  for (let i = 2; i < n; i++) {
+    if (array[i]) {
+      count++;
+    }
+  }
+
+  return count;
 };
 
 module.exports = countPrimes;
