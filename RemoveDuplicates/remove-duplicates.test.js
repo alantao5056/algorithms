477cda32af1ec8f72039f36826135992ad36f3e7
Jian Tao <taojian@live.com>
2019-11-20 15:40:24 -0500

    Add remove duplicates

---
+++ RemoveDuplicates/remove-duplicates.test.js
@@ -0,0 +1,33 @@
+const removeDuplicates = require('./remove-duplicates');
+
+describe('removeDuplicates', () => {
+
+  test('0,0,1,1,2,3,4,4,4,5', () => {
+    let numbers = [0, 0, 1, 1, 2, 3, 4, 4, 4, 5];
+    const length = removeDuplicates(numbers);
+    numbers.splice(length);
+    expect(numbers).toEqual([0, 1, 2, 3, 4, 5]);
+  });
+
+  test('1', () => {
+    let numbers = [1];
+    const length = removeDuplicates(numbers);
+    numbers.splice(length);
+    expect(numbers).toEqual([1]);
+  });
+
+  test('3,3,3,3,3,3', () => {
+    let numbers = [3, 3, 3, 3, 3, 3];
+    const length = removeDuplicates(numbers);
+    numbers.splice(length);
+    expect(numbers).toEqual([3]);
+  });
+
+  test('1,3,3,3,3,3,3', () => {
+    let numbers = [1, 3, 3, 3, 3, 3, 3];
+    const length = removeDuplicates(numbers);
+    numbers.splice(length);
+    expect(numbers).toEqual([1, 3]);
+  });
+
+});
