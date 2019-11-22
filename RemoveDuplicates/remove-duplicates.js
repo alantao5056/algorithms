2bba637ce411580f80be5a8652ddb718a9113061
Alan Tao <alantao5056@gmail.com>
2019-11-21 19:16:33 -0500

    Finished remove duplicates but needed improvement

--- RemoveDuplicates/remove-duplicates.js
+++ RemoveDuplicates/remove-duplicates.js
@@ -3,7 +3,21 @@
  * @return {number}
  */
 const removeDuplicates = function (nums) {
-
+  let count = 0;
+  let i = 0;
+  let end = nums.length - 1;
+  while (i < end) {
+    if (nums[i] === nums[i + 1]) {
+      for (let j = i + 2; j < nums.length; j++) {
+        nums[j - 1] = nums[j];
+      }
+      end--;
+    } else {
+      count++;
+      i++;
+    }
+  }
+  return count + 1;
 };
 
 module.exports = removeDuplicates;
--- RemoveDuplicates/remove-duplicates.test.js
+++ RemoveDuplicates/remove-duplicates.test.js
@@ -16,6 +16,13 @@ describe('removeDuplicates', () => {
     expect(numbers).toEqual([1]);
   });
 
+  test('1 3 3 5', () => {
+    let numbers = [1, 3, 3, 5];
+    const length = removeDuplicates(numbers);
+    numbers.splice(length);
+    expect(numbers).toEqual([1, 3, 5]);
+  });
+
   test('3,3,3,3,3,3', () => {
     let numbers = [3, 3, 3, 3, 3, 3];
     const length = removeDuplicates(numbers);
